7f8130bbc336d448114cee840d256cf270be6a43
stkrolikiewicz <46401055+stkrolikiewicz@users.noreply.github.com>
2024-11-19 19:54:43 +0100

    feature: create RootLayout with Navigation and replace App with it

--- src/main.tsx
+++ src/main.tsx
@@ -3,13 +3,29 @@ import { createRoot } from "react-dom/client";
 import { ThemeProvider } from "@mui/material/styles";
 import { CssBaseline } from "@mui/material";
 import theme from "./theme";
-import App from "./App.tsx";
+import { RootLayout } from "@/layouts";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import { HomePage, RecommendationsPage } from "@/pages";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+      <Route index element={<HomePage />} />
+      <Route path="recommendations" element={<RecommendationsPage />} />
+    </Route>
+  )
+);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <App />
+      <RouterProvider router={router} />
     </ThemeProvider>
   </StrictMode>
 );
