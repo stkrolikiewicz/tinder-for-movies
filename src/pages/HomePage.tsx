ec3a0eb58e40fb6b4299d477dccd6cba07bb9dbb
stkrolikiewicz <46401055+stkrolikiewicz@users.noreply.github.com>
2024-11-19 17:03:09 +0100

    feature: provide browser router and pages

---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,8 @@
+import type { FC } from "react";
+import { Typography } from "@mui/material";
+
+const HomePage: FC = () => {
+  return <Typography variant="h1">Home Page</Typography>;
+};
+
+export default HomePage;
