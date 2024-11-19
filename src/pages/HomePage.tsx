44eec9debcba46321c5edc8627d94ec8df5d73b5
stkrolikiewicz <46401055+stkrolikiewicz@users.noreply.github.com>
2024-11-19 21:43:26 +0100

    feature: add home page content

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,42 @@
 import type { FC } from "react";
-import { Typography } from "@mui/material";
+import { useNavigate } from "react-router-dom";
+import { Button, Box, Typography } from "@mui/material";
+import { styled } from "@mui/system";
+
+const Container = styled(Box)({
+  display: "flex",
+  flexDirection: "column",
+  alignItems: "center",
+  justifyContent: "center",
+  height: "100vh",
+  textAlign: "center",
+});
 
 const HomePage: FC = () => {
-  return <Typography variant="h1">Home Page</Typography>;
+  const navigate = useNavigate();
+
+  const handleRedirect = () => {
+    navigate("/recommendations", {
+      viewTransition: true,
+      state: { fromHomePage: true },
+    });
+  };
+
+  return (
+    <Container>
+      <Typography variant="h3" gutterBottom>
+        Welcome to Tinder for Movies
+      </Typography>
+      <Button
+        variant="contained"
+        size="large"
+        color="primary"
+        onClick={handleRedirect}
+      >
+        Go to Recommendations
+      </Button>
+    </Container>
+  );
 };
 
 export default HomePage;
