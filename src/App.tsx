4d13d33005abd68d94b63c27a82b9d16df4aa213
stkrolikiewicz <46401055+stkrolikiewicz@users.noreply.github.com>
2024-11-19 17:03:09 +0100

    feature: provide browser router and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HomePage, RecommendationsPage } from "@/pages";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/recommendations",
+    element: <RecommendationsPage />,
+  },
+]);
+
 function App() {
-  return <>Tinder For Movies</>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/RecommendationsPage.tsx
@@ -0,0 +1,8 @@
+import type { FC } from "react";
+import { Typography } from "@mui/material";
+
+const RecommendationsPage: FC = () => {
+  return <Typography variant="h1">Recommendations Page</Typography>;
+};
+
+export default RecommendationsPage;
