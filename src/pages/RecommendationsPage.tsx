db11ddafb3c459e0f1166cb2ef0354450cfc0acc
stkrolikiewicz <46401055+stkrolikiewicz@users.noreply.github.com>
2024-11-20 23:49:44 +0100

    feature: create MovieCard component

--- src/pages/RecommendationsPage.tsx
+++ src/pages/RecommendationsPage.tsx
@@ -1,37 +1,90 @@
 import type { FC } from "react";
-import { Typography } from "@mui/material";
-// import { useRecommentations } from "@/context/RecommendationsContext";
-import { recommendations } from "@/mock/recommendations";
+import { Typography, Button, Container } from "@mui/material";
+import { useRecommentations } from "@/context/RecommendationsContext";
+// import { recommendations } from "@/mock/recommendations";
+import { MovieCard } from "@/components/recommendations";
+import theme from "@/theme";
+import { ThumbDown, ThumbUp } from "@mui/icons-material";
 
 const RecommendationsPage: FC = () => {
-  // const { recommendations, isLoading, error } = useRecommentations();
+  const {
+    currentRecommendation,
+    // acceptRecommendation,
+    // rejectRecommendation,
+    moveToNextRecommendation,
+    isLoading,
+    error,
+  } = useRecommentations();
 
-  // if (isLoading) {
-  //   return <Typography variant="h3">Loading...</Typography>; // TODO: add skeleton loader or spinner
-  // }
+  if (isLoading) {
+    return <Typography variant="h3">Loading...</Typography>; // TODO: add skeleton loader or spinner
+  }
 
-  // if (error) {
-  //   return <Typography variant="h3">{error.message}</Typography>; // TODO: add error component
-  // }
+  if (error) {
+    return <Typography variant="h3">{error.message}</Typography>; // TODO: add error component
+  }
 
-  // if (!recommendations.length) {
-  //   return <Typography variant="h3">No recommendations found</Typography>; // TODO: add empty state
-  // }
+  if (!currentRecommendation) {
+    return <Typography variant="h3">No more recommendationns</Typography>; // TODO: add empty state
+  }
+
+  const handleAccept = () => {
+    // await acceptRecommendation(currentRecommendation.id);
+    moveToNextRecommendation();
+  };
+
+  const handleReject = () => {
+    // await rejectRecommendation(currentRecommendation.id);
+    moveToNextRecommendation();
+  };
 
   return (
-    <div>
-      {/* TODO: Add container component */}
-      <Typography variant="h3">Recommendations</Typography>
-      {/* TODO: movies carousel carousel */}
-      <div>
-        {recommendations.map((recommendation) => (
-          /* TODO: Add movie card component */
-          <div key={recommendation.id}>
-            <Typography variant="h2">{recommendation.title}</Typography>
-          </div>
-        ))}
-      </div>
-    </div>
+    <Container
+      disableGutters
+      sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        flex: 1,
+        gap: 2,
+        [theme.breakpoints.up("sm")]: {
+          maxHeight: "calc(100vh - 64px - 48px)",
+        },
+        [theme.breakpoints.down("sm")]: {
+          maxHeight: "calc(100vh - 56px - 48px)",
+        },
+      }}
+    >
+      <MovieCard key={currentRecommendation.id} item={currentRecommendation} />
+      <Container
+        maxWidth="sm"
+        disableGutters
+        sx={{
+          display: "flex",
+          gap: 2,
+          justifyContent: "space-between",
+        }}
+      >
+        <Button
+          onClick={handleAccept}
+          variant="outlined"
+          color="success"
+          startIcon={<ThumbUp />}
+          size="large"
+        >
+          Accept
+        </Button>
+        <Button
+          onClick={handleReject}
+          variant="outlined"
+          color="error"
+          endIcon={<ThumbDown />}
+          size="large"
+        >
+          Reject
+        </Button>
+      </Container>
+    </Container>
   );
 };
 
