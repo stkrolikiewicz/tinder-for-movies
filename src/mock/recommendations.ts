6473adfdc2296d51d94aa11b69d0d7676c6870bb
stkrolikiewicz <46401055+stkrolikiewicz@users.noreply.github.com>
2024-11-20 20:14:16 +0100

    feature: implement mock recommendations

---
+++ src/mock/recommendations.ts
@@ -0,0 +1,84 @@
+import { Recommendation } from "@/types/recommendations";
+
+export const recommendations: Recommendation[] = [
+  {
+    id: "1and3011",
+    imageURL:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTUzNTE2NTkzMV5BMl5BanBnXkFtZTgwMDAzOTUyMDI@._V1_SY1000_CR0,0,674,1000_AL_.jpg",
+    title: "Inferno",
+    summary: "Lorem ipsum….",
+    rating: 5.3,
+  },
+  {
+    id: "2301abc",
+    imageURL:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BOTAzODEzNDAzMl5BMl5BanBnXkFtZTgwMDU1MTgzNzE@._V1_SY1000_CR0,0,677,1000_AL_.jpg",
+    title: "Star Wars: Episode VII - The Force Awakens",
+    summary: "Lorem ipsum….",
+    rating: 8.2,
+  },
+  {
+    id: "3def456",
+    imageURL:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMjQzMjQ2NjY5N15BMl5BanBnXkFtZTgwNzY2NjQ3NzE@._V1_SY1000_CR0,0,674,1000_AL_.jpg",
+    title: "The Martian",
+    summary: "Lorem ipsum….",
+    rating: 8.0,
+  },
+  {
+    id: "4ghi789",
+    imageURL:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMjM1OTM0MzY2MF5BMl5BanBnXkFtZTgwNzY2NjQ3NzE@._V1_SY1000_CR0,0,674,1000_AL_.jpg",
+    title: "Mad Max: Fury Road",
+    summary: "Lorem ipsum….",
+    rating: 8.1,
+  },
+  {
+    id: "5jkl012",
+    imageURL:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMjM1OTM0MzY2MF5BMl5BanBnXkFtZTgwNzY2NjQ3NzE@._V1_SY1000_CR0,0,674,1000_AL_.jpg",
+    title: "Jurassic World",
+    summary: "Lorem ipsum….",
+    rating: 7.0,
+  },
+  {
+    id: "6mno345",
+    imageURL:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMjM1OTM0MzY2MF5BMl5BanBnXkFtZTgwNzY2NjQ3NzE@._V1_SY1000_CR0,0,674,1000_AL_.jpg",
+    title: "Inside Out",
+    summary: "Lorem ipsum….",
+    rating: 8.5,
+  },
+  {
+    id: "7pqr678",
+    imageURL:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMjM1OTM0MzY2MF5BMl5BanBnXkFtZTgwNzY2NjQ3NzE@._V1_SY1000_CR0,0,674,1000_AL_.jpg",
+    title: "Spectre",
+    summary: "Lorem ipsum….",
+    rating: 6.8,
+  },
+  {
+    id: "8stu901",
+    imageURL:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMjM1OTM0MzY2MF5BMl5BanBnXkFtZTgwNzY2NjQ3NzE@._V1_SY1000_CR0,0,674,1000_AL_.jpg",
+    title: "Mission: Impossible - Rogue Nation",
+    summary: "Lorem ipsum….",
+    rating: 7.4,
+  },
+  {
+    id: "9vwx234",
+    imageURL:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMjM1OTM0MzY2MF5BMl5BanBnXkFtZTgwNzY2NjQ3NzE@._V1_SY1000_CR0,0,674,1000_AL_.jpg",
+    title: "Ant-Man",
+    summary: "Lorem ipsum….",
+    rating: 7.3,
+  },
+  {
+    id: "10yz567",
+    imageURL:
+      "https://images-na.ssl-images-amazon.com/images/M/MV5BMjM1OTM0MzY2MF5BMl5BanBnXkFtZTgwNzY2NjQ3NzE@._V1_SY1000_CR0,0,674,1000_AL_.jpg",
+    title: "Avengers: Age of Ultron",
+    summary: "Lorem ipsum….",
+    rating: 7.3,
+  },
+];
